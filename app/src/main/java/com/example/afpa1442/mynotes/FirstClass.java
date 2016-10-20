304a5b65eadaca4c32d8e7687b7914b86e780094
Ibrahim <brahms.sama@gmail.com>
2016-10-20 16:27:39 +0200

    Commit n°2

--- app/src/main/java/com/example/afpa1442/mynotes/FirstClass.java
+++ app/src/main/java/com/example/afpa1442/mynotes/FirstClass.java
@@ -1,8 +1,92 @@
 package com.example.afpa1442.mynotes;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Ibrahim on 20/10/2016.
  */
 
-public class FirstClass {
+public class FirstClass extends AppCompatActivity {
+        String result="";
+
+        String resultat="";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public class Asynchrone extends AsyncTask<String,Integer,String> {
+            @Override
+            protected String doInBackground(String... params) {
+                HttpURLConnection httpUrlConnection = null;
+                try {
+
+                    URL url = new URL("http://10.75.25.52/tp09webservices/testwebservice.php/5");
+                    httpUrlConnection = (HttpURLConnection)
+                            url.openConnection();
+                    InputStream inStream = new
+                            BufferedInputStream(httpUrlConnection.getInputStream());
+                    int inChar;
+                    final StringBuilder readStr = new StringBuilder();
+                    while ((inChar = inStream.read()) != -1) {
+                        readStr.append((char) inChar);
+                    }
+                    result = readStr.toString();
+                    parseJsonFile(result);
+                    httpUrlConnection.disconnect();
+                } catch (MalformedURLException me) {
+                    me.printStackTrace();
+                } catch (IOException io) {
+                    io.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return resultat;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        public void reception(View v){
+            resultat="";
+            com.example.afpa1442.mynotes.MainActivity.Asynchrone as=new com.example.afpa1442.mynotes.MainActivity.Asynchrone();
+            as.execute("ok");
+        }
+
+        private void parseJsonFile (String jString) throws Exception {
+            JSONObject jsonObj = new JSONObject(jString);
+
+            JSONArray note = jsonObj.getJSONArray("notes");
+            for (int i = 0; i < note.length(); i++) {
+                String attributeId = note.getJSONObject(i).getString("id");
+                resultat+="id: "+attributeId+"\n";
+                String attributeName = note.getJSONObject(i).getString("title");
+                resultat+="title: "+attributeName+"\n";
+                String attributeAuthor = note.getJSONObject(i).getString("text");
+                resultat+="text: "+attributeAuthor+"\n";
+                String attributeIsbn = note.getJSONObject(i).getString("solved");
+                resultat+="solved: "+attributeIsbn+"\n";
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/afpa1442/mynotes/MainActivity.java
+++ app/src/main/java/com/example/afpa1442/mynotes/MainActivity.java
@@ -1,87 +1,101 @@
 package com.example.afpa1442.mynotes;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.Toast;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity {
-
-    String result="";
-    String resultat="";
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    public class Asynchrone extends AsyncTask<String,Integer,String> {
-        @Override
-        protected String doInBackground(String... params) {
-            HttpURLConnection httpUrlConnection = null;
-            try {
-
-                URL url = new URL("http://10.75.25.52/tp09webservices/testwebservice.php/5");
-                httpUrlConnection = (HttpURLConnection)
-                        url.openConnection();
-                InputStream inStream = new
-                        BufferedInputStream(httpUrlConnection.getInputStream());
-                int inChar;
-                final StringBuilder readStr = new StringBuilder();
-                while ((inChar = inStream.read()) != -1) {
-                    readStr.append((char) inChar);
-                }
-                result = readStr.toString();
-                parseJsonFile(result);
-                httpUrlConnection.disconnect();
-            } catch (MalformedURLException me) {
-                me.printStackTrace();
-            } catch (IOException io) {
-                io.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-            return resultat;
-        }
+        });
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
-    public void reception(View v){
-        resultat="";
-        Asynchrone as=new Asynchrone();
-        as.execute("ok");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    private void parseJsonFile (String jString) throws Exception {
-        JSONObject jsonObj = new JSONObject(jString);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        JSONArray note = jsonObj.getJSONArray("notes");
-        for (int i = 0; i < note.length(); i++) {
-            String attributeId = note.getJSONObject(i).getString("id");
-            resultat+="id: "+attributeId+"\n";
-            String attributeName = note.getJSONObject(i).getString("title");
-            resultat+="title: "+attributeName+"\n";
-            String attributeAuthor = note.getJSONObject(i).getString("text");
-            resultat+="text: "+attributeAuthor+"\n";
-            String attributeIsbn = note.getJSONObject(i).getString("solved");
-            resultat+="solved: "+attributeIsbn+"\n";
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }

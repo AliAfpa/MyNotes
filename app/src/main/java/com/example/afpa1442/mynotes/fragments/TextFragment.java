4f3a6f529d24bc455d03b1e591892b38ddff1dfb
Ibrahim <brahms.sama@gmail.com>
2016-10-25 13:31:36 +0200

    Commit n°2654

--- app/src/main/java/com/example/afpa1442/mynotes/fragments/TextFragment.java
+++ app/src/main/java/com/example/afpa1442/mynotes/fragments/TextFragment.java
@@ -1,9 +1,34 @@
 package com.example.afpa1442.mynotes.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.afpa1442.mynotes.NoteApplication;
+import com.example.afpa1442.mynotes.R;
+import com.example.afpa1442.mynotes.classes.Note;
 
 /**
  * Created by Ibrahim on 25/10/2016.
  */
 public class TextFragment extends Fragment {
+
+    Note note;
+    private NoteApplication application;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.bottom_fragment, container, false);
+
+        application = (NoteApplication) getActivity().getApplication();
+        note = new Note();
+        TextView tv = (TextView) rootView.findViewById(R.id.noteText);
+        tv.setText("TEXTE DE LA REMARQUE");
+
+        return rootView;
+    }
+
 }
